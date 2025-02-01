5b9fd3549ac4a5677dc6c96fe999fdabd71db57a
shrirambansal <shrirambansal13@gmail.com>
2025-02-01 11:31:01 +0530

    Fix database URI configuration

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 app = Flask(__name__)
 
 # Secure Database Connection
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")  # Use Railway Environment Variable
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")  # Use Railway Environment Variable
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
